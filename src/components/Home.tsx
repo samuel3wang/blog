4621d5ac352941884df76e921da6f8e75ba5c456
Samuel Wang <yuanyuan030303@gmail.com>
2022-12-20 23:39:31 +0800

    personal card

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Card from './Card'
 
 const HomePage = () => {
     return(
         <div>
             <p>Home</p>
+            <Card></Card>
         </div>
     )
 }
