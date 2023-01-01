9fbd4c8317760de358c1bdd8a69b4dbaf8832f7a
Samuel Wang <yuanyuan030303@gmail.com>
2023-01-01 09:11:23 +0800

    use tailwindcss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 export default function Header() {
   return (
     <header>
-      <h1 >Samuel's Notes</h1>
+      <h1 className="text-3xl font-bold underline">Samuel's Notes</h1>
     </header>
   )
 }
